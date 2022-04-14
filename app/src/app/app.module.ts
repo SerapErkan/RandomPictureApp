78c914f576b882e7619e5b80d62627b2330ae9b9
SERAP KANTAROĞLU <serapkantaroglu.59@hotmail.com>
2022-04-15 00:30:18 +0300

    DisplayphotoComponent

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayphotoComponent } from './displayphoto/displayphoto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayphotoComponent
   ],
   imports: [
     BrowserModule,
